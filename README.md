a41f0febef49c08e6e896139f3425f43a8d107b7
h34tn <heath.luo@gmail.com>
2019-06-28 11:32:04 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# ys-script-language README
+
+This script language is for YangShan private used.
+
+## Features
+
+Highlight script keywords.
+Support snippets.
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,13 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext)
+{
+    // vscode.languages.registerHoverProvider(
+    //     { scheme: 'untitled', language: 'ysscript' },
+    //     {
+    //       provideHover(doc: vscode.TextDocument) {
+    //         return new vscode.Hover('For new, unsaved YS Script documents only');
+    //       }
+    //     }
+    //   );    
+}
