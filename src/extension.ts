19f4ca26b7963a22f1f38e128d190428b8e96101
h34tn <heath.luo@gmail.com>
2019-09-19 10:42:33 +0800

    update

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,6 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext)
 {
-    // vscode.languages.registerHoverProvider(
-    //     { scheme: 'untitled', language: 'ysscript' },
-    //     {
-    //       provideHover(doc: vscode.TextDocument) {
-    //         return new vscode.Hover('For new, unsaved YS Script documents only');
-    //       }
-    //     }
-    //   );    
+    
 }
+
